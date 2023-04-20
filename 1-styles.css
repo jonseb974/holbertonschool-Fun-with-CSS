add04169e744c2e72b80b590dccee038a02b05e8
jonseb974 <5151@holbertonstudents.com>
2023-04-20 09:32:14 +0200

    add file 1-styles

---
+++ 1-styles.css
@@ -0,0 +1,26 @@
+/* Hide the default underline */
+h2 a { 
+    color: red;
+    text-decoration: none; 
+    position: relative;
+}
+
+
+h2 a:after { 
+    content:"";
+    display: block;
+    height: 2px;
+    width: 0;
+    background-color: red;
+    position: absolute;
+    bottom: -2px;
+    left: 0;
+    transition: width 0.5s ease; 
+}
+
+/* Gradually reveal the underline effect on hover */
+
+h2 a:hover:after {
+    width: 100%;
+}
+
